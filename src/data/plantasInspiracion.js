42a25638f5b4008687cab094ca0f06e270a9584a
LuckyTom1610 <j240484@gmail.com>
2025-09-30 12:51:54 -0500

    Prueba directa con nombres simples en getImage

--- src/data/plantasInspiracion.js
+++ src/data/plantasInspiracion.js
@@ -1,78 +1,79 @@
+import { getImage } from "../utils/cloudinary";
 const plantasInspiracion = [
   {
     id: "planta-1",
     nombre: "Calatea",
-    imagen: "/images/Plantas/Calatea.jpg",
-  },
+    imagen: getImage("Calatea.png"),
+    },
   {
     id: "planta-2",
     nombre: "Cheflera",
-    imagen: "/images/Plantas/Cheflera.jpg",
+    imagen: getImage("Cheflera.png"),
   },
   {
     id: "planta-3",
     nombre: "Clavelón--Marigold",
-    imagen: "/images/Plantas/Clavelón--Marigold.jpg",
+    imagen: getImage("Clavelón--Marigold.png"),
   },
   {
     id: "planta-4",
     nombre: "Costilla de Adán",
-    imagen: "/images/Plantas/Costilla de Adán.jpg",
+    imagen: getImage("Costilla de Adán.png"),
   },
   {
     id: "planta-5",
     nombre: "Croto",
-    imagen: "/images/Plantas/Croto.jpg",
+    imagen: getImage("Croto.png"),
   },
   {
     id: "planta-6",
     nombre: "Drácena",
-    imagen: "/images/Plantas/Drácena.jpg",
+    imagen: getImage("Drácena.png"),
   },
   {
     id: "planta-7",
     nombre: "Ficus caucho",
-    imagen: "/images/Plantas/Ficus caucho.jpg",
+    imagen: getImage("Ficus caucho.png"),
   },
   {
     id: "planta-8",
     nombre: "Ficus lira o violín",
-    imagen: "/images/Plantas/Ficus lira o violín.jpg",
+    imagen: getImage("Ficus lira o violín.png"),
   },
   {
     id: "planta-9",
     nombre: "Filodendro hoja grande",
-    imagen: "/images/Plantas/Filodendro hoja grande.jpg",
+    imagen: getImage("Filodendro hoja grande.png"),
   },
   {
     id: "planta-10",
     nombre: "Lavanda",
-    imagen: "/images/Plantas/Lavanda.jpg",
+    imagen: getImage("Lavanda.png"),
   },
   {
     id: "planta-11",
     nombre: "Mini calatea",
-    imagen: "/images/Plantas/Mini calatea.jpg",
+    imagen: getImage("Mini calatea.png"),
   },
   {
     id: "planta-12",
     nombre: "Palma cica",
-    imagen: "/images/Plantas/Palma cica.jpg",
+    imagen: getImage("Palma cica.png"),
   },
   {
     id: "planta-13",
     nombre: "Peperomia",
-    imagen: "/images/Plantas/Peperomia.jpg",
+    imagen: getImage("Peperomia.png"),
   },
   {
     id: "planta-14",
     nombre: "Planta china del dinero",
-    imagen: "/images/Plantas/Planta china del dinero.jpg",
+    imagen: getImage("Planta china del dinero.png"),
   },
   {
     id: "planta-15",
     nombre: "Trébol morado",
-    imagen: "/images/Plantas/Trébol morado.jpg",
+    imagen: getImage("Trébol_morado.png"),
   },
 ];
 
