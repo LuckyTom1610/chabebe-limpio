3a1c9c218ad93491ff562c8455df1130b813b915
LuckyTom1610 <j240484@gmail.com>
2025-09-30 20:03:08 -0500

    actualización Componentes

--- src/components/setdemacetasPage.jsx
+++ src/components/setdemacetasPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import CatalogoMacetas from "./CatalogoMacetas";
 import { CartContext } from "../context/CartContext";
 import { getImage } from "../utils/cloudinary";
