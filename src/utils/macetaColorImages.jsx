374d20b3402734479520d4fbaa5e1fce6d9a2a31
LuckyTom1610 <j240484@gmail.com>
2025-10-02 11:21:55 -0500

    actualización componente macetaColorIamges

--- src/utils/macetaColorImages.jsx
+++ src/utils/macetaColorImages.jsx
@@ -23,45 +23,45 @@ export const macetaColorImages = {
   },
 
 GraciaLowBetonEffect: {
-    "422U": getImage("Gracia_Low_Beton Effect_DGRL400LE_422U.jpg"),
+    "422U": getImage("Gracia_Low_Beton Effect_DGRL400LE_422U.png"),
     "CY700": getImage("Gracia_Low_Beton_Effect_DGRL400LE_CY700.png"),
     "S433": getImage("Gracia_Low_Beton_Effect_DGRL400LE_S433.png"),
   },
   
       GraciaLow: {
-        "S449": getImage("Gracia_Low_DGRL400L_S449.jpg"),
-        "443U": getImage("Gracia_Low_DGRL400L_443U.jpg"),
-        "S433": getImage("Gracia_Low_DGRL400L_S433.jpg"),
-        "5615C": getImage("Gracia_Low_DGRL400L_5615C.jpg"),
+        "S449": getImage("Gracia_Low_DGRL400L_S449.png"),
+        "443U": getImage("Gracia_Low_DGRL400L_443U.png"),
+        "S433": getImage("Gracia_Low_DGRL400L_S433.png"),
+        "5615C": getImage("Gracia_Low_DGRL400L_5615C.png"),
   },
 
       GRACIALOWECOWOOD: {
         "S449W": getImage("Gracia_Low_ECO_Wood_DGRL400LW_S449W_m.png"),
-        "4665W": getImage("Gracia_Low_ECO_Wood_DGRL400LW_4665W_naturo.jpg"),
-        "S433W": getImage("Gracia_Low_ECO_Wood_DGRL400LW_S433W.jpg"),
-        "4625W": getImage("Gracia_Low_Eco_Wood_DGRL400LW_4625W.jpg"),
+        "4665W": getImage("Gracia_Low_ECO_Wood_DGRL400LW_4665W_naturo.png"),
+        "S433W": getImage("Gracia_Low_ECO_Wood_DGRL400LW_S433W.png"),
+        "4625W": getImage("Gracia_Low_Eco_Wood_DGRL400LW_4625W.png"),
 },
       GRACIATUBUSSLIMECOWOOD: {
         "S449W": getImage("Gracia_Tubus_Slim_ECO_Wood_DGTL240W_S449W_m.png"),
-        "4665W": getImage("Gracia_Tubus_Slim_ECO_Wood_DGTL240W_4665W.jpg"),
-        "S433W": getImage("Gracia_Tubus_Slim_ECO_Wood_DGTL240W_S433W.jpg"),
-        "4625W": getImage("Gracia_Tubus_Slim_ECO_Wood_DGTL240W_4625W.jpg"),
+        "4665W": getImage("Gracia_Tubus_Slim_ECO_Wood_DGTL240W_4665W.png"),
+        "S433W": getImage("Gracia_Tubus_Slim_ECO_Wood_DGTL240W_S433W.png"),
+        "4625W": getImage("Gracia_Tubus_Slim_ECO_Wood_DGTL240W_4625W.png"),
   },
       GRACIASQUAREECOWOOD: {
-        "S449W": getImage("Gracia_Square_ECO_Wood_DGQL400W_S449W.jpg"),
-        "4665W": getImage("Gracia_Square_ECO_Wood_DGQL400W_4665W.jpg"),
-        "S433W": getImage("Gracia_Square_ECO_Wood_DGQL400W_S433W.jpg"),
-        "4625W": getImage("Gracia_Square_ECO_Wood_DGQL400W_4625W.jpg"),
+        "S449W": getImage("Gracia_Square_ECO_Wood_DGQL400W_S449W.png"),
+        "4665W": getImage("Gracia_Square_ECO_Wood_DGQL400W_4665W.png"),
+        "S433W": getImage("Gracia_Square_ECO_Wood_DGQL400W_S433W.png"),
+        "4625W": getImage("Gracia_Square_ECO_Wood_DGQL400W_4625W.png"),
   },
 
           TUBOPM: {
-        "S449": getImage("Tubo_PM_DPOD_S449.jpg"),
+        "S449": getImage("Tubo_PM_DPOD_S449.png"),
         "2411U": getImage("Tubo_PM_DPOD400_2411U.png"),
-        "S433": getImage("Tubo_PM_DPOD_443U.jpg"),
+        "S433": getImage("Tubo_PM_DPOD_443U.png"),
           },
            GRACIASQUARE: {
         "S449": getImage("Gracia_square_DGQL400_S449.png"),
-        "443U": getImage("Gracia_square_DGQL400_443U.jpg"),
+        "443U": getImage("Gracia_square_DGQL400_443U.png"),
         "5615C": getImage("Gracia_square_DGQL400_5615C.png"),
         "S433": getImage("Gracia_square_DGQL400_S433.png"),
           },
@@ -94,11 +94,11 @@ GraciaLowBetonEffect: {
             },
               DMIN200: {
             "4985U": getImage("Milly_DMIN200_4985U_m.png"),
-            "S449": getImage("Milly_Round_DMIN200_S449.jpg"),
-            "443U": getImage("Milly_Round_DMIN200_443U.jpg"),
-            "2411U": getImage("Milly_Round_DMIN200_2411U.jpg"),
-            "S433": getImage("Milly_Round_DMIN200_S433.jpg"),
-            "446U": getImage("Milly_Round_DMIN200_446U.jpg"),
+            "S449": getImage("Milly_Round_DMIN200_S449.png"),
+            "443U": getImage("Milly_Round_DMIN200_443U.png"),
+            "2411U": getImage("Milly_Round_DMIN200_2411U.png"),
+            "S433": getImage("Milly_Round_DMIN200_S433.png"),
+            "446U": getImage("Milly_Round_DMIN200_446U.png"),
             },
           },
         },
@@ -111,7 +111,7 @@ GraciaLowBetonEffect: {
             "S433": getImage("DKSP180_S433 SPLOFY.png"),
                   },
               DKSP290: {
-            "7529U": getImage("DKSP290_7529U SPLOFY.jpg"),
+            "7529U": getImage("DKSP290_7529U SPLOFY.png"),
             "S449": getImage("DKSP290_S449 SPLOFY.png"),
             "S433": getImage("DKSP290_S433 SPLOFY.png"),
                   },
@@ -140,16 +140,16 @@ GraciaLowBetonEffect: {
           HEOS: {
             variantes: {
               DLHEN470: {
-            S449: getImage("Heos_DLHEN470_S449.jpg"),
-            S433: getImage("Heos_DLHEN470_S433.jpg"),
+            S449: getImage("Heos_DLHEN470_S449.png"),
+            S433: getImage("Heos_DLHEN470_S433.png"),
       },
               DBHEN470: {
-            S449: getImage("Heos_DBHEN470_S449.jpg"),
-            S433: getImage("Heos_DBHEN470_S433_m.jpg"),
+            S449: getImage("Heos_DBHEN470_S449.png"),
+            S433: getImage("Heos_DBHEN470_S433_m.png"),
       },
               DBHER470: {
-            S449: getImage("Heos_DBHER_S449.jpg"),
-            S433: getImage("Heos_DBHER_S433.jpg"),
+            S449: getImage("Heos_DBHER_S449.png"),
+            S433: getImage("Heos_DBHER_S433.png"),
       },
     },
   },
@@ -172,7 +172,7 @@ GraciaLowBetonEffect: {
             S449: getImage("Grower_DOGR200_S449.png"),
             },
               DOGR240: {
-            S449: getImage("Grower_DOGR240_S449.jpg"),
+            S449: getImage("Grower_DOGR240_S449.png"),
        },
      },
    },
@@ -182,7 +182,7 @@ GraciaLowBetonEffect: {
 
                  MillyWall: {
             "S449": getImage("Milly_Wall_DMIW150_S449_m.png"),
-            "443U": getImage("Milly_Wall_DMIW150_443U.jpg"),
+            "443U": getImage("Milly_Wall_DMIW150_443U.png"),
             "2411U": getImage("Milly_Wall_DMIW150_2411U_m.png"),
             "4985U": getImage("Milly_Wall_DMIW150_4985U_m.png"),
             "446U": getImage("Milly_Wall_DMIW150_446U_m.png"),
@@ -198,27 +198,27 @@ GraciaLowBetonEffect: {
                 CORObowl: {
                  variantes: {
               TCB40H: {
-            "7529U": getImage("Coro_Bowl_High_TCB40H_101GR.jpg"),
-            "443U": getImage("Coro_Bowl_High_TCB40H_107GR_graphite.jpg"),
+            "7529U": getImage("Coro_Bowl_High_TCB40H_101GR.png"),
+            "443U": getImage("Coro_Bowl_High_TCB40H_107GR_graphite.png"),
             "405U": getImage("Coro_Bowl_High_TCB40H_109GR_m.png"),
                   },
               TCB48: {
-            "7529U": getImage("Coro_Bowl_TCB48_101GR.jpg"),
-            "443U": getImage("Coro_Bowl_TCB48_107GR_graphite.jpg"),
-            "405U": getImage("Coro_Bowl_TCB48_109GR.jpg"),
+            "7529U": getImage("Coro_Bowl_TCB48_101GR.png"),
+            "443U": getImage("Coro_Bowl_TCB48_107GR_graphite.png"),
+            "405U": getImage("Coro_Bowl_TCB48_109GR.png"),
                   },
                  },
         }, 
               MOLTAsquarefoot: {
                  variantes: {
               TMSF40: {
-            "7529U": getImage("Molta_Square_Foot_TMSF40_101GR.jpg"),
-            "443U": getImage("Molta_Square_Foot_TMSF40_107GR.jpg"),
+            "7529U": getImage("Molta_Square_Foot_TMSF40_101GR.png"),
+            "443U": getImage("Molta_Square_Foot_TMSF40_107GR.png"),
             
                   },
               TMSF48: {
-            "7529U": getImage("Molta_Square_Foot_TMSF48_101GR.jpg"),
-            "443U": getImage("Molta_Square_Foot_TMSF48_107GR.jpg"),
+            "7529U": getImage("Molta_Square_Foot_TMSF48_101GR.png"),
+            "443U": getImage("Molta_Square_Foot_TMSF48_107GR.png"),
                 },
               },
             }, 
@@ -227,7 +227,7 @@ GraciaLowBetonEffect: {
                variantes: {
               TDS30: {
             "7529U": getImage("Duala_Round_Slim_TDR30S_101GR_m.png"),
-            "443U": getImage("Duala_Round_Slim_TDR30S_107GR.jpg"),
+            "443U": getImage("Duala_Round_Slim_TDR30S_107GR.png"),
             
                   },
               TDS40: {
@@ -248,16 +248,16 @@ GraciaLowBetonEffect: {
   },
 
   GRACIACASEBETONEFFECT: {
-    "422U": getImage("Gracia_Case_Beton_Effect_DGCL600E_422U.jpg"),
-    "CY700": getImage("Gracia_Case_Beton_Effect_DGCL600E_CY700.jpg"),
-    "S433": getImage("Gracia_Case_Beton_Effect_DGCL600E_S433.jpg"),
+    "422U": getImage("Gracia_Case_Beton_Effect_DGCL600E_422U.png"),
+    "CY700": getImage("Gracia_Case_Beton_Effect_DGCL600E_CY700.png"),
+    "S433": getImage("Gracia_Case_Beton_Effect_DGCL600E_S433.png"),
   },
 
       GRACIACASEECOWOOD: {
-    "S449W": getImage("Gracia_Case_ECO_Wood_DGCL600W_S449W.jpg"),
-    "4665W": getImage("Gracia_Case_ECO_Wood_DGCL600W_4665W.jpg"),
-    "S433W": getImage("Gracia_Case_ECO_Wood_DGCL600W_S433W.jpg"),
-    "4625W": getImage("Gracia_Case_ECO_Wood_DGCL600W_4625W.jpg"),
+    "S449W": getImage("Gracia_Case_ECO_Wood_DGCL600W_S449W.png"),
+    "4665W": getImage("Gracia_Case_ECO_Wood_DGCL600W_4665W.png"),
+    "S433W": getImage("Gracia_Case_ECO_Wood_DGCL600W_S433W.png"),
+    "4625W": getImage("Gracia_Case_ECO_Wood_DGCL600W_4625W.png"),
   },
       GRACIACASE: {
     "S449": getImage("Gracia_Case_DGCL400_S449.png"),
@@ -266,7 +266,7 @@ GraciaLowBetonEffect: {
     "S433": getImage("Gracia_Case_DGCL400_S433_m.png"),
   },
   CASCADEGRADENWALL: {
-    "S433": getImage("Garden_Long_Wall_IO3W800_2 modulowy_S433.jpg"),
+    "S433": getImage("Garden_Long_Wall_IO3W800_2 modulowy_S433.png"),
   },
      BLOMA: {
     "405U": getImage("DBLO600_1.png"),
@@ -281,21 +281,21 @@ GraciaLowBetonEffect: {
      HEOS: {
     variantes: {
       DLHEN470: {
-        S449: getImage("Heos_DLHEN470_S449.jpg"),
-        S433: getImage("Heos_DLHEN400_S433.jpg"),
+        S449: getImage("Heos_DLHEN470_S449.png"),
+        S433: getImage("Heos_DLHEN400_S433.png"),
       },
       DBHEN470: {
-        S449: getImage("Heos_DBHEN470_S449.jpg"),
-        S433: getImage("Heos_DBHEN470_S433_m.jpg"),
+        S449: getImage("Heos_DBHEN470_S449.png"),
+        S433: getImage("Heos_DBHEN470_S433_m.png"),
       },
       DBHER470: {
-        S449: getImage("Heos_DBHER_S449.jpg"),
-        S433: getImage("Heos_DBHER_S433.jpg"),
+        S449: getImage("Heos_DBHER_S449.png"),
+        S433: getImage("Heos_DBHER_S433.png"),
       },
     },
   },
       RESPANAPLANTERWOODHIGHSET: {
-    "405U": getImage("ISEW780H_1.jpg"),
+    "405U": getImage("ISEW780H_1.png"),
   },
       ARIAOVAL: {
     variantes: {
@@ -306,41 +306,41 @@ GraciaLowBetonEffect: {
       },
       TAO60: {
         "7529U": getImage("Aria_Oval_TAO60_101GR_sand_m.png"),
-        "443U": getImage("Aria_Oval_TAO60_105GR.jpg"),
-        "405U": getImage("Aria_Oval_TAO60_107GR_graphite.jpg"),
+        "443U": getImage("Aria_Oval_TAO60_105GR.png"),
+        "405U": getImage("Aria_Oval_TAO60_107GR_graphite.png"),
       },
       },
   },    
         CORObowlhigh: {
       variantes: {
         TCB40H: {
-        "7529U": getImage("Coro_Bowl_High_TCB40H_101GR.jpg"),
-        "443U": getImage("Coro_Bowl_High_TCB40H_107GR_graphite.jpg"),
-        "405U": getImage("Coro_Bowl_High_TCB40H_109GR_m.jpg"),
+        "7529U": getImage("Coro_Bowl_High_TCB40H_101GR.png"),
+        "443U": getImage("Coro_Bowl_High_TCB40H_107GR_graphite.png"),
+        "405U": getImage("Coro_Bowl_High_TCB40H_109GR_m.png"),
       },
         TCB48: {
-        "7529U": getImage("Coro_Bowl_TCB48_101GR.jpg"),
-        "443U": getImage("Coro_Bowl_TCB48_107GR_graphite.jpg"),
-        "405U": getImage("Coro_Bowl_TCB48_109GR.jpg"),
+        "7529U": getImage("Coro_Bowl_TCB48_101GR.png"),
+        "443U": getImage("Coro_Bowl_TCB48_107GR_graphite.png"),
+        "405U": getImage("Coro_Bowl_TCB48_109GR.png"),
       },
       },
       },
           EVOLIA: {
-        "7529U": getImage("Evolia_TEV60_101GR_sand.jpg"),
-        "443U": getImage("Evolia_TEV60_105GR.jpg"),
-        "405U": getImage("Evolia_TEV60_107GR_graphite.jpg"),
+        "7529U": getImage("Evolia_TEV60_101GR_sand.png"),
+        "443U": getImage("Evolia_TEV60_105GR.png"),
+        "405U": getImage("Evolia_TEV60_107GR_graphite.png"),
           },
 
           YEKE: {
         "7529U": getImage("Yeke_TY50_101GR_sand_m.png"),
-        "443U": getImage("Yeke_TY50_105GR_tiny_granite.jpg"),
-        "405U": getImage("Yeke_TY50_107GR_graphite.jpg"),
+        "443U": getImage("Yeke_TY50_105GR_tiny_granite.png"),
+        "405U": getImage("Yeke_TY50_107GR_graphite.png"),
            },
 
            MOLTA: {
-        "7529U": getImage("Molta_Round_Foot_TMRF60_101GR.jpg"),
-        "443U": getImage("Molta_Round_Foot_TMRF60_105GR.jpg"),
-        "405U": getImage("Molta_Round_Foot_TMRF60_107GR.jpg"),
+        "7529U": getImage("Molta_Round_Foot_TMRF60_101GR.png"),
+        "443U": getImage("Molta_Round_Foot_TMRF60_105GR.png"),
+        "405U": getImage("Molta_Round_Foot_TMRF60_107GR.png"),
             },
 
 
